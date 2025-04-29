3f49c8fcf3ecb30f2211e63686ab5e0e67b10ce7
zoebailey <113046758+zoebailey@users.noreply.github.com>
2025-04-28 20:40:47 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 330L</h1>
+      <h2>Section - 011</h2>
+      <p>WVU ID: 800391807</p>
+      <p>Hi I am Zoe</p>
+    </div>
+  );
+}
+
+export default App;
